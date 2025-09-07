26f6e0731b69376c9b5b4ad0b4d46ca8325e6214
rishilmastin2007-maker <rishilmastin2007@gmail.com>
2025-09-07 15:05:22 +0530

    updated the file

--- arrays/reversuingwithoutusiingextrarry.cpp
+++ arrays/reversuingwithoutusiingextrarry.cpp
@@ -1,15 +1,31 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int a[]={12,6,7,34,9,230,2,8};
-    int n= sizeof(a)/4;
-    int i=0;
-    int j=n-1;
-    while (i<j){
-        int temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
+
+int main() {
+    int a[] = {12, 6, 7, 34, 9, 230, 2, 8};
+    int n = sizeof(a) / sizeof(a[0]); // Correct way to get array size
+    int i = 0;
+    int j = n - 1;
+    
+    cout << "Original array: ";
+    for (int k = 0; k < n; k++) {
+        cout << a[k] << " ";
+    }
+    cout << endl;
+    
+    while (i < j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
         i++;
         j--;
     }
+    
+    cout << "Reversed array: ";
+    for (int k = 0; k < n; k++) {
+        cout << a[k] << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
