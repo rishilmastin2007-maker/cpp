ccf6621116e939697f17dec6b22d6c3bc8d28555
rishilmastin2007-maker <rishilmastin2007@gmail.com>
2025-09-07 15:41:21 +0530

    added the file

---
+++ 2D Array/marks_roll.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4][2]={{76,25},{63,54},{55,36},{57,58}};
+    for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
